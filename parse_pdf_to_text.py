5eb881d0d37f7ef9fd086adee443eaea1e28029d
lenik terenin <lenik@lazydroid.com>
2016-12-20 14:08:45 +0900

    pdf parser with subfolder support

--- parse_pdf_to_text.py
+++ parse_pdf_to_text.py
@@ -7,31 +7,52 @@ using the "pdftotext" command. If a pdf cannot be converted, this
 script will not produce the output file.
 """
 
-import cPickle as pickle
-import urllib2
-import shutil
+#import cPickle as pickle
+#import urllib2
+#import shutil
 import time
 import os
-import random
+#import random
 
 os.system('mkdir -p txt') # ?
 
-have = set(os.listdir('txt'))
-files = os.listdir('pdf')
-for i,f in enumerate(files, start=1):
-  pdf_path = os.path.join('pdf', f)
-  txt_basename = f + '.txt'
-  txt_path = os.path.join('txt', txt_basename)
-  if not txt_basename in have:
-    cmd = "pdftotext %s %s" % (pdf_path, txt_path)
-    os.system(cmd)
-    print '%d/%d %s' % (i, len(files), cmd)
+#have = set(os.listdir('txt'))
 
-    # check output was made
-    if not os.path.isfile(txt_path):
-      # there was an error with converting the pdf
-      os.system('touch ' + txt_path) # create empty file, but it's a record of having tried to convert
+have = []
+for r, d, files in os.walk('txt') :
+	for f in files :
+		if f.endswith('.txt') :
+			have.append(f)
+have = set(have)
 
-    time.sleep(0.02) # silly way for allowing for ctrl+c termination
-  else:
-    print 'skipping %s, already exists.' % (pdf_path, )
+
+for r, d, files in os.walk('pdf') :
+	for f in files :
+		if not f.endswith('.pdf') : continue
+
+		folder = r.replace('pdf', 'txt')
+		if not os.path.isdir(folder) :
+			os.mkdir( folder )
+
+		pdf_path = os.path.join( r, f)
+		file_size = os.path.getsize( pdf_path )
+		if file_size < 10000 :
+			print 'too small to be a pdf: %s %d' % (pdf_path, file_size)
+			#os.unlink( pdf_path )
+			continue
+
+		txt_basename = f.replace('pdf', 'txt')
+		txt_path = pdf_path.replace('pdf', 'txt')
+		if not txt_basename in have :
+			cmd = "pdftotext %s %s" % (pdf_path, txt_path)
+			print '%s' % cmd
+			os.system(cmd)
+
+			# check output was made
+			if not os.path.isfile(txt_path):
+				# there was an error with converting the pdf
+				os.system('touch ' + txt_path) # create empty file, but it's a record of having tried to convert
+
+			time.sleep(0.02) # silly way for allowing for ctrl+c termination
+		else:
+			print 'skipping %s, already exists.' % (pdf_path, )
